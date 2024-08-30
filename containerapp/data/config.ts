c44f8925549d3c11e2c16f46262b00fd4f7af8e6
Lama1403 <121100574+Lama1403@users.noreply.github.com>
2024-08-30 18:42:03 +0300

    Update config.ts

--- containerapp/data/config.ts
+++ containerapp/data/config.ts
@@ -1,8 +1,8 @@
 import { Config } from "./src/config";
 
 export const defaultConfig: Config = {
-  url: "https://www.builder.io/c/docs/developers",
-  match: "https://www.builder.io/c/docs/**",
+  url: "https://developer-portal.zilstg.dev/zilliqa2/",
+  match: "https://developer-portal.zilstg.dev/zilliqa2/**",
   maxPagesToCrawl: 50,
   outputFileName: "../data/output.json",
 };
