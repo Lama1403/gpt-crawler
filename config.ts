d517133de0689465fded9d81dc23a1f54e34e5a7
Lama1403 <121100574+Lama1403@users.noreply.github.com>
2024-08-30 18:33:07 +0300

    e

--- config.ts
+++ config.ts
@@ -2,9 +2,8 @@ import { Config } from "./src/config";
 
 export const defaultConfig: Config = {
   url: "https://developer-portal.zilstg.dev/zilliqa2/",
-  match: "https://developer-portal.zilstg.dev/zilliqa2/**",
+  match: "https://developer-portal.zilstg.dev/zilliqa2/**/",
   maxPagesToCrawl: 100,
   outputFileName: "output.json",
   maxTokens: 2000000,
 };
-
