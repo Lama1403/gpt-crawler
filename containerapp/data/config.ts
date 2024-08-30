0911e7d227168b26471a191b3ddafa2ced316c28
Lama1403 <121100574+Lama1403@users.noreply.github.com>
2024-08-30 18:53:44 +0300

    the

--- containerapp/data/config.ts
+++ containerapp/data/config.ts
@@ -2,7 +2,12 @@ import { Config } from "./src/config";
 
 export const defaultConfig: Config = {
   url: "https://developer-portal.zilstg.dev/zilliqa2/",
-  match: "https://developer-portal.zilstg.dev/zilliqa2/**",
+  match: ["https://developer-portal.zilstg.dev/zilliqa2/**"],// Changed to array
   maxPagesToCrawl: 50,
   outputFileName: "../data/output.json",
+  // Other optional or required properties
+  // exclude: ["/path/to/exclude"],
+  // selector: "body",
+  // cookie: { name: "session", value: "xyz" },
 };
+
